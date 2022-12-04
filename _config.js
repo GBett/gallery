859324d70357a3c351af3a32114de3244036e060
GBett <118375989+GBett@users.noreply.github.com>
2022-12-04 16:20:51 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -1,9 +1,8 @@
-var config = {}
-
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<Gbett>:<bett123>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<Gbett>:<bett123>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<Gbett>:<bett123>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
-module.exports = config;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Gilbert:<bett123>@atlascluster.7kdi3zi.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
