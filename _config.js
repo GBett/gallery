16f1c40954fae6a1e61a7a4fcd7a429648d3649a
GBett <gilbertkalo@yahoo.com>
2022-12-14 15:23:39 +0300

    updated database URL

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Gilbert:<bett123>@atlascluster.7kdi3zi.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Gilbert:<bett123>@atlascluster.7kdi3zi.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Gilbert:<bett123>@atlascluster.7kdi3zi.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Gilbert:bett123@bett.xrkcvln.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Gilbert:bett123@bett.xrkcvln.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Gilbert:bett123@bett.xrkcvln.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // connecting the database
 
 let mongodb_url =
-"mongodb+srv://Gilbert:<password>@bett.xrkcvln.mongodb.net/?retryWrites=true&w=majority";
+"mongodb+srv://Gilbert:bett123@bett.xrkcvln.mongodb.net/?retryWrites=true&w=majority";
 let dbName = "darkroom";
 mongoose.connect(`${mongodb_url}${dbName}`,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -45,7 +45,7 @@ app.use('/image', image);
 
 
  
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
